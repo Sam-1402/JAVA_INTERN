593e97363860c296f53788aeae69df862a79ec2a
SAMYAK JAIN <99808230+Sam-1402@users.noreply.github.com>
2023-07-01 11:19:59 +0530

    Update Main.java

--- Similarity_Algorithms/Main.java
+++ Similarity_Algorithms/Main.java
@@ -5,27 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Size of Vectors: ");
-        int n = sc.nextInt();
-
-        System.out.println("Enter the Vector 1: ");
-        double[] vector1 = new double[n];
-        for(int i=0; i<n; i++){
-            vector1[i] = sc.nextInt();
-        }
-
-        System.out.println("Enter the Vector 2: ");
-        double[] vector2 = new double[n];
-        for(int i=0; i<n; i++){
-            vector2[i] = sc.nextInt();
-        }
-
         System.out.println("Choose Algorithms: (1-Cosine Similarity, 2-Euclidean Distance, 3-Manhattan Distance, 4-Jaccard Similarity, 5-Minkowski Distance)");
-        String algos[] = {"cosine", "euclidean", "manhattan", "jaccard", "minkoski"};
+        String algos[] = {"cosine", "euclidean", "manhattan", "jaccard", "minkowski"};
         int num = sc.nextInt();
         Similarities sm = Similarity_Adapter.getSimilarityAlgo(algos[num-1]);
-        sm.vector1 = vector1;
-        sm.vector2 = vector2;
+        sm.inputFilename1 = "C:\\Users\\asus\\IdeaProjects\\internship\\src\\Similarity_Algorithms\\Dummy_text2.txt";
+        sm.inputFilename2 = "C:\\Users\\asus\\IdeaProjects\\internship\\src\\Similarity_Algorithms\\Dummy_text3.txt";
         System.out.println("Similarity: "+sm.findSimilarity());
     }
 }
