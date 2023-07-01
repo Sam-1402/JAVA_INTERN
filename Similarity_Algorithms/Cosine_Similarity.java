baeeb6e43edd105da42551f726421becdb54b5e5
SAMYAK JAIN <99808230+Sam-1402@users.noreply.github.com>
2023-07-01 11:18:32 +0530

    Update Cosine_Similarity.java

--- Similarity_Algorithms/Cosine_Similarity.java
+++ Similarity_Algorithms/Cosine_Similarity.java
@@ -1,17 +1,27 @@
 package Similarity_Algorithms;
 
+import java.util.ArrayList;
+
+import static Similarity_Algorithms.Vectorizer.ConverttoVector;
 import static java.lang.Math.sqrt;
 
 public class Cosine_Similarity extends Similarities{
     @Override
     public double findSimilarity() {
-        System.out.println("Using Cosine Similarity Formula");
-        int n = vector1.length;
+        System.out.println("Using Cosine Similarity Formula: ");
+        System.out.println("Obtaining Vectors...");
+
+        ArrayList<Double> vector1 = new ArrayList<>();
+        ArrayList<Double> vector2 = new ArrayList<>();
+        ConverttoVector(inputFilename1, inputFilename2, vector1, vector2);
+
+        int n = vector1.size();
 
         //first we will obtain x.y
         double xy = 0;
+
         for(int i=0; i<n; i++){
-            xy = xy + (vector1[i]*vector2[i]);
+            xy = xy + (vector1.get(i)*vector2.get(i));
         }
 
         //Now we will obtain the ||X|| x ||Y||
@@ -20,14 +30,13 @@ public class Cosine_Similarity extends Similarities{
         double sumx = 0;
         double sumy = 0;
         for(int i=0; i<n; i++){
-            sumx = sumx + (vector1[i]*vector1[i]);
-            sumy = sumy + (vector2[i]*vector2[i]);
+            sumx = sumx + (vector1.get(i)*vector1.get(i));
+            sumy = sumy + (vector2.get(i)*vector2.get(i));
         }
         modx = sqrt(sumx);
         mody = sqrt(sumy);
 
         double XxY = modx*mody;
-
         double cosineRatio = xy/XxY;
 
         return cosineRatio;
